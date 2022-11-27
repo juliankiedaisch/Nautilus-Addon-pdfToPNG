b61ac72c0dfbe772b53a24832fb73a8094b2eed9
juliankiedaisch <julian@kiedaisch.net>
2022-11-27 22:01:30 +0100

    Update nautlus-pdfToPNG.py
    Added Support for Nautilus 43

--- nautlus-pdfToPNG.py
+++ nautlus-pdfToPNG.py
@@ -2,28 +2,32 @@ import subprocess
 import urllib.parse
 from gi.repository import Nautilus, GObject
 from pdf2image import convert_from_path
+from typing import List
 
 SUPPORTED_FORMATS = 'application/pdf'
 
 class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
-        pass
+        super().__init__()
+        print("Initialized test extension")
 
-    def menu_activate_cb(self, menu, files):
-        for file in files:
-            pages = convert_from_path(urllib.parse.unquote(file.get_uri().replace("file://", "")), 500)
+    def menu_activate_cb(self, menu: Nautilus.MenuItem, files: List[Nautilus.FileInfo],) -> List[Nautilus.MenuItem]:
+        for item in files:
+            pages = convert_from_path(urllib.parse.unquote(item.get_uri().replace("file://", "")), 500)
             for index in range(len(pages)):
-                pages[index].save(urllib.parse.unquote(file.get_uri().replace("file://", "").split(".")[0] + "_" + str(index) + ".png"), 'PNG')
-    def get_file_items(self, window, files):
+                pages[index].save(urllib.parse.unquote(item.get_uri().replace("file://", "").split(".")[0] + "_" + str(index) + ".png"), 'PNG')
+    def get_file_items(self, files: List[Nautilus.FileInfo],) -> List[Nautilus.MenuItem]:
+
         for item in files:
             if not item.get_mime_type() in SUPPORTED_FORMATS:
-                return
+                return []
             if item.get_uri_scheme() != 'file':
-                return
+                return []
 
         item = Nautilus.MenuItem(
-            name="ExampleMenuProvider::FileCopy", label="PDF in PNG umwandeln", tip="", icon=""
+            name="SimpleMenuExtension::FileCopy", label="PDF in PNG umwandeln", tip="", icon=""
         )
         item.connect("activate", self.menu_activate_cb, files)
-        return (item,)
+        return [item,]
 
+    def get_background_items(self, current_folder: Nautilus.FileInfo,) -> List[Nautilus.MenuItem]: return []
